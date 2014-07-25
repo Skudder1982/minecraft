e6981a8569390c7d3cb21e653478905ce6471092
Skudder1982 <skudder1982@gmail.com>
2014-07-25 11:28:35 +0200

    .

--- opencomputers/robots/test.lua
+++ opencomputers/robots/test.lua
@@ -3,71 +3,71 @@ local quarrySize = 11;
 local timerEvent;
 
 function placeQuarry()
-	robot.up();
-	robot.select(14);
-	robot.placeDown();
-	robot.up();
-	robot.select(15);
-	robot.placeDown();
-	robot.up();
-	robot.select(16);
-	robot.placeDown();
+    robot.up();
+    robot.select(14);
+    robot.placeDown();
+    robot.up();
+    robot.select(15);
+    robot.placeDown();
+    robot.up();
+    robot.select(16);
+    robot.placeDown();
 end
 
 function digQuarry()
-	robot.select(16);
-	robot.swingDown();
-	robot.down();
-	robot.select(15);
-	robot.swingDown();
-	robot.down();
-	robot.select(14);
-	robot.swingDown();
-	robot.down();
+    robot.select(16);
+    robot.swingDown();
+    robot.down();
+    robot.select(15);
+    robot.swingDown();
+    robot.down();
+    robot.select(14);
+    robot.swingDown();
+    robot.down();
 end
 
 function clearInv()
-	for i = 1,14 do
-	  robot.select(i);
-	  robot.drop();
-	end
+    for i = 1,14 do
+      robot.select(i);
+      robot.drop();
+    end
 end
 
 function move()
-	for i = 1,quarrySize do
-		robot.back();
-	end
+    for i = 1,quarrySize do
+        robot.back();
+    end
 end
 
 function work(isFirstTime)
-	if not isFirstTime then
-		digQuarry();
-		clearInv();
-		move();
-	end
-	
-	placeQuarry();
+    if not isFirstTime then
+        digQuarry();
+        clearInv();
+        move();
+    end
+    
+    placeQuarry();
 end
 
 function timerCallback()
-	work(true);
-	os.sleep(10);
-	work(false);
-	os.sleep(10);
-	digQuarry();
-	clearInv();
+    work(true);
+    os.sleep(10);
+    work(false);
+    os.sleep(10);
+    digQuarry();
+    clearInv();
 end
 
 function start()
---	work(true);
---	timerEvent = event.timer(15, timerCallback);
+--  work(true);
+--  timerEvent = event.timer(15, timerCallback);
 
-	work(true);
-	os.sleep(10);
-	work(false);
-	os.sleep(10);
-	digQuarry();
-	clearInv();
+    work(true);
+    os.sleep(10);
+    work(false);
+    os.sleep(10);
+    digQuarry();
+    clearInv();
 end
 
 start();
